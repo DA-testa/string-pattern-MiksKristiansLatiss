40bf8682fa219f9fd52c642aecfce19f7250bde1
MiksKristiansLatiss <125253937+MiksKristiansLatiss@users.noreply.github.com>
2023-04-17 00:17:07 +0300

    test9

--- hash_substring.py
+++ hash_substring.py
@@ -3,18 +3,19 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    input_type = input().rstrip()
+    input_type = input()[0]
 
     if input_type == 'F':
-        file_name = input().rstrip()
-        with open(file_name) as f:
-            pattern = f.readline().rstrip()
-            text = f.readline().rstrip()
+        input_file = "06"
+        input_file = "tests/" + input_file
+        with open(input_file) as f:
+            pattern = f.readline().strip()
+            text = f.readline().strip()
     elif input_type == 'I':
-        pattern = input().rstrip()
-        text = input().rstrip()
+        pattern = input().strip()
+        text = input().strip()
     else:
-        return
+        return None
     
     # after input type choice
     # read two lines 
@@ -28,27 +29,36 @@ def read_input():
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
-    print(' '.join(map(str, output)))
+    print(*occurrences)
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
+    q = len(pattern)
+    w = len(text)
+    p = 10**9 + 7
+    x = 263
+    h = pow(x, q-1, p)
+    pattern_hash = sum(ord(pattern[i])* pow(x, i, p) for i in range(q)) % p
+    text_hash = sum(ord(text[i]) * pow(x, i, p) for i in range(q)) % p
     occurrences = []
-    p_len = len(pattern)
-    t_len = len(text)
-    p_hash = sum(ord(pattern[i]) * (101 ** i) for i in range(p_len))
-    t_hash = sum(ord(text[i]) * (101 ** i) for i in range(p_len))
 
-    for i in range(t_len - p_len + 1):
-        if p_hash == t_hash:
-            if pattern == text[i:i+p_len]:
+    for i in range(w-q+1):
+        if pattern_hash == text_hash:
+            if pattern == text[i:i+q]:
                 occurrences.append(i)
-        if i < t_len - p_len:
-            t_hash = (t_hash - ord(text[i]) * (101 ** (p_len - 1))) * 101 + ord(text[i + p_len])
+        if i < w-q:
+            text_hash = (text_hash - ord(text[i]) * h) % p
+            text_hash = (text_hash * x + ord(text[i+q])) % p
+
     # and return an iterable variable
     return occurrences
 
 
 # this part launches the functions
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
+    input_data = read_input()
+    if input_data:
+        pattern, text = input_data
+        occurrences = get_occurrences(pattern, text)
+        print_occurrences(occurrences)
 
